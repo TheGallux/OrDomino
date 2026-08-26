f883f4004731831538bb2c07f6a267164d461815
Clement CHOLLET <clement.chollet@epita.fr>
2026-08-26 20:27:18 +0200

    feat: added an 'Hello World\!'

---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+OrDomino entry point
+"""
+
+
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+logger.info("Starting 'OrDomino'")
+
+print("Hello World!")
+
+logger.info("Finished 'OrDomino'")
